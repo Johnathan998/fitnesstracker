762520b39b1813b793b4f3a17eb8b30525f5bcc9
johnathan olivas <jj.galvan13@yahoo.com>
2020-03-28 21:33:36 -0600

    got PUT to work

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -20,7 +20,7 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-    Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true, runValidators: true, })
+    Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
